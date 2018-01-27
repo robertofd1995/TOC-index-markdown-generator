34c46805edc25f14129d855f41e90fa71f610890
Roberto Fernandez Diaz <robertofd1995@users.noreply.github.com>
2018-01-27 14:45:43 +0100

    Solve problem with codification

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 import os
+import codecs
 
-name_of_file= "index.md"
-route = "/Users/UserName/Documents/Folder"
-deepLevel=1
+name_of_file = "index.md"
+route = r'C:\Users\rober\Documents\Github\My_Voto_Documentation'
+deepLevel = 1
 
 
 def create_element_list(file_name):
-
     myFile.write("* [" + file_name.replace(".md", "") + "]" + "(/" + file_name + ")\n")
 
-def create_header_list(route, deep_level):
 
-    myFile.write("#" * deep_level + route + "\n")
+def create_header_list(route, deep_level):
+    str = "#" * deep_level + route + "\n"
+    myFile.write(str)
+
 
 def write_files_in_directory(list_of_files):
-
     for file in list_of_files:
         if file.endswith(".md"):
             create_element_list(file)
             print(file)
 
+
 def write_index_of_directory(route, deep_level):
-
-
-    if deep_level==1:
-        myLife = open(name_of_file, "a")
+    if deep_level == 1:
+        myLife = codecs.open(name_of_file, "a", "utf-8-sig")
 
         # Si ya existe un fichero TOC lo eliminamos
         if os.stat(name_of_file).st_size != 0:
             os.remove(name_of_file)
 
-    if deep_level!=1:
+    if deep_level != 1:
         # Vamos a la ruta de donde queremos generar el indice
         os.chdir(route)
 
-    deep_level_now=deep_level
-
-
+    deep_level_now = deep_level
 
     create_header_list(os.path.relpath(".", ".."), deep_level)
 
@@ -45,7 +43,6 @@ def write_index_of_directory(route, deep_level):
     # Explora por archivos
 
     if listOfFiles:
-
         print(" \nfiles : ")
         write_files_in_directory(listOfFiles)
 
@@ -53,7 +50,7 @@ def write_index_of_directory(route, deep_level):
 
     if listOfDirs:
 
-    # Explora por directorios y ir hacia ellos  y repetir
+        # Explora por directorios y ir hacia ellos  y repetir
 
         print("\nDIRS : ")
         for dir in listOfDirs:
@@ -61,21 +58,15 @@ def write_index_of_directory(route, deep_level):
             route_directory = os.path.join(route, dir)
 
             if os.path.isdir(route_directory):
-
-                deep_level_now+=1
+                deep_level_now += 1
 
                 write_index_of_directory(route_directory, deep_level_now)
 
 
-
 if __name__ == '__main__':
-
-
     os.chdir(route)
-    myFile = myLife = open(name_of_file, "a")
+    myFile = codecs.open(name_of_file, "a", "utf-8-sig")
 
     write_index_of_directory(route, deepLevel)
 
     myFile.close()
-
-
